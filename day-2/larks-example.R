be0fc089e12103cc8068867d3cf9aea893a664ab
Gavin Simpson <ucfagls@gmail.com>
2022-10-11 12:53:49 +0200

    add some example scripts

---
+++ day-2/larks-example.R
@@ -0,0 +1,99 @@
+# Portugese Larks example from slides
+
+# packages
+library("readr")
+library("dplyr")
+library("mgcv")
+library("gratia")
+library("patchwork")
+
+# read in the data
+larks_url <- "https://bit.ly/gam-larks"
+larks <-  read_csv(larks_url, col_types = "ccdddd")
+
+# Or as per the slides:
+# larks <-  read_csv(here("data", "larks.csv"),
+#                   col_types = "ccdddd")
+
+# convert some variables to factors (two bird species), and scale the
+# easting and northing variables to be in KM
+# numerically we prefer smaller valued variables
+larks <- larks %>%
+  mutate(crestlark = factor(crestlark),
+         linnet = factor(linnet),
+         e = x / 1000,
+         n = y / 1000)
+larks
+
+# plot the data
+larks %>%
+    ggplot(aes(x = e, y = n, colour = crestlark)) +
+    geom_point(size = 0.5) + # add a point layer
+    coord_fixed() +
+    scale_colour_discrete(na.value = "#bbbbbb33") +
+    labs(x = NULL, y = NULL)
+
+# fit our model
+# isotropic thin plate regression spline smooth of spatial coordinates
+crest <- gam(crestlark ~ s(e, n, k = 100),
+             data = larks,
+             family = binomial,
+             method = "REML")
+
+# model summary
+summary(crest)
+
+# visualise the estimate smooth
+# a grid of 75x75 is a bit more efficient than the default of 100x100
+draw(crest, rug = FALSE, n = 75)
+
+# model checking is a pain for 0/1 data, so aggregate to binomial counts
+larks2 <- larks %>%
+  mutate(crestlark = as.numeric(as.character(crestlark)),  # to numeric
+         linnet = as.numeric(as.character(linnet)),
+         tet_n = rep(1, nrow(larks)), # counter for how many grid cells we sum
+         N = rep(1, nrow(larks)),     # number of obs, 1 per row currently
+         N = if_else(is.na(crestlark), NA_real_, N)) %>% # set N to NA if no obs
+  group_by(QUADRICULA) %>%                     # group by the larger grid square
+  summarise(across(c(N, crestlark, linnet, tet_n, e, n),
+                   ~ sum(., na.rm = TRUE))) %>%  # sum all needed variables
+  mutate(e = e / tet_n, n = n / tet_n) # rescale to get avg E,N coords
+
+# fit binomial GAM
+crest2 <- gam(cbind(crestlark, N - crestlark) ~ s(e, n, k = 100),
+    data = larks2,
+    family = binomial,
+    method = "REML")
+
+# model summary
+summary(crest2)
+
+# compare the fits
+draw(crest, n = 75, rug = FALSE) +
+    draw(crest2, n = 75, rug = FALSE) +
+    plot_layout(ncol = 2)
+# look pretty similar
+
+# let's look at some model diagnostics
+appraise(crest2, method = "simulate")
+
+# hmmm, some overdispersion - refit as a quasi binomial
+crest3 <- gam(cbind(crestlark, N - crestlark) ~ s(e, n, k = 100),
+    data = larks2,
+    family = quasibinomial,
+    method = "REML")
+
+# look at the estimate dispersion parameter - this should be 1 for true
+# binomial counts
+crest3$scale # should be == 1
+
+# model summary
+summary(crest3)
+
+# let's look at some model diagnostics
+appraise(crest2, method = "simulate")
+
+# compare the fits
+draw(crest, n = 75, rug = FALSE) +
+    draw(crest3, n = 75, rug = FALSE) +
+    plot_layout(ncol = 2)
---
+++ day-2/south-pole-co2-example.R
@@ -0,0 +1,84 @@
+# South Pole CO2 example from slides
+
+# packages
+library("readr")
+library("dplyr")
+library("mgcv")
+library("gratia")
+
+south_url <- "https://bit.ly/gam-south-pole-co2"
+south <- read_csv(south_url, col_types = "ddd")
+south
+
+# co2 is the CO2 concentration in PPM
+# c.month is a monthly counter 1, 2, M, where M is the last n_year * 12 month
+#   alternatively just think of this as a timestep variable
+# month is a numeric indicator of month of year
+
+# plot the data
+ggplot(south, aes(x = c.month, y = co2)) +
+    geom_line()
+
+# Fit a naive GAM using the time step variable
+# we need really large k to capture the within and between year variability
+# in a single smooth
+m_co2 <- gam(co2 ~ s(c.month, k = 300, bs = "cr"),
+    data = south, method = "REML")
+
+# model summary
+summary(m_co2)
+
+# let's predict from this naive model
+# this requires a new data frame of values we want to predict at
+# lets also forecast the next few years (36 months)
+new_df <- with(south, tibble(c.month = 1:(nrow(south) + 36)))
+
+# look at our data
+new_df
+
+# then we use the fitted_values function from gratia to generate predictions
+fv <- fitted_values(m_co2, data = new_df, scale = "response")
+
+# look at fv
+fv
+
+# plot our predictions
+fv %>%
+    ggplot(aes(x = c.month, y = fitted)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+    geom_line(data = south, aes(c.month, co2), col = "red") +
+    geom_line(alpha = 0.4)
+
+# linear extrapolation - be very careful extrapolating with splines...
+
+# a better model would decompose into seasonal variation and long term trend
+# ideally we'd do this with `year` and `month` variables but here we just have
+# a time variable (`c.month` and a `month`` variable)
+# Note how we set the knots
+m2_co2 <- gam(co2 ~ s(month, bs = "cc") +
+                s(c.month, bs = "cr", k = 300),
+    data = south, method = "REML",
+    knots = list(month = c(0.5, 12.5)))
+
+# model summary
+summary(m2_co2)
+
+# plot these two smooths
+draw(m2_co2, residuals = TRUE, rug = FALSE)
+
+# compare their complexities
+model_edf(m_co2, m2_co2)
+
+# what about our forecasts
+nr <- nrow(south)
+new_df <- with(south,
+               tibble(c.month = 1:(nr + 36),
+                      month = rep(seq_len(12), length = nr + 36)))
+fv2 <- fitted_values(m2_co2, data = new_df, scale = "response")
+
+# and plot
+fv2 %>%
+ggplot(aes(x = c.month, y = fitted)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+    geom_line(data = south, aes(c.month, co2), col = 'red') +
+    geom_line(alpha = 0.4)
